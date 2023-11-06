59652b91903f58e86fe685b4cf5ec5357914e0d3
Andre Alves <andresistinfo@gmail.com>
2023-11-06 06:49:35 +0000

    Adding delete quesiton comment use case

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -2,4 +2,6 @@ import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
   create(question: QuestionComment): Promise<void>
+  delete(question: QuestionComment): Promise<void>
+  findById(id: string): Promise<QuestionComment>
 }
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -9,4 +9,16 @@ export class InMemoryQuestionCommentRepository
   async create(questionComments: QuestionComment): Promise<void> {
     this.items.push(questionComments)
   }
+
+  async delete(question: QuestionComment): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(id: string): Promise<QuestionComment> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    return questionComment as QuestionComment
+  }
 }
