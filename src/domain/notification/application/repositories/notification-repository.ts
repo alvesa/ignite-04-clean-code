4f1331ef57d83481ef13f30a557b94114dd4f62a
Andre Alves <andresistinfo@gmail.com>
2023-11-16 07:16:48 +0000

    Creating read notification use case

--- src/domain/notification/application/repositories/notification-repository.ts
+++ src/domain/notification/application/repositories/notification-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../../enterprise/entitites/notification'
 
 export interface NotificationRepository {
   create(notification: Notification): Promise<void>
+  findById(id: string): Promise<Notification | null>
+  save(notification: Notification): Promise<void>
 }
